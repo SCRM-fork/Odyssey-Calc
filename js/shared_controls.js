782bb56780beae196f07e7698e97b53ed0a6fbe2
SCRM-fork <johanneswild93@gmx.de>
2025-08-17 17:38:18 +0200

    Hide IVs

--- js/shared_controls.js
+++ js/shared_controls.js
@@ -516,10 +516,10 @@ function smogonAnalysis(pokemonName) {
 function refresh_next_in() {
 	var next_poks = get_next_in()
 
-	//if (damageGen < 7 && !TITLE.includes("Lumi") && damageGen != 1) {
-    //    $("#p2 .evs, #p2 .ev-label").hide()
+	if (damageGen == 3 ) {
+        $("#p2 .ivs, #p2 .iv-label, #p1 .ivs, #p1 .iv-label").hide()
 
-    //}
+    }
 
     if (damageGen == 1) {
     	$('.evs.calc-trigger').attr('max', '65535').addClass('expanded')
